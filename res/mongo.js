ebc2ebccdc18e8a90cfb07b5e686435b3aadd262
Phan M. Hung <pmhungpro1@gmail.com>
2022-09-18 15:14:58 +0700

    update

--- res/mongo.js
+++ res/mongo.js
@@ -1,13 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL = "mongodb://localhost:27017";
-
+const MONGO_URL = 'mongodb://localhost:27017';
 
 var database = MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-    .then((connection) => {
-        var method = connection.db("essence").collection('method');
-        console.log("Database connection established")
+  .then((connection) => {
+    var method = connection.db('essence').collection('method');
+    console.log('Database connection established');
+  })
+  .catch((err) => console.error(err));
 
-    })
-    .catch((err) => console.error(err))
-
-module.exports = database
+module.exports = database;
